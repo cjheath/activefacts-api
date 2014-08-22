0c4a1b6827447407dbc8f8894790b2cfec0b9d62
Clifford Heath <clifford.heath@gmail.com>
2014-08-22 16:25:34 +1000

    Provided random GUID generation with fixed-sequence, record and replay options, for repeatable testing and debugging

--- lib/activefacts/api/guid.rb
+++ lib/activefacts/api/guid.rb
@@ -1,22 +1,41 @@
 require 'delegate'
 require 'securerandom'
 
+def SecureRandom.format_uuid hex32
+  hex32.sub(
+    @@format_pattern ||= /(........)(....)(....)(....)(............)/,
+    @@format_string ||= '\1-\2-\3-\4-\5'
+  )
+end
+
 unless defined? SecureRandom.uuid
   # I think this only applies to 1.8.6 (and JRuby/Rubinius in 1.8 mode) now:
   def SecureRandom.uuid
-    hex(16).
-      sub(
-	@@format_pattern ||= /(........)(....)(....)(....)(............)/,
-	@@format_string ||= '\1-\2-\3-\4-\5'
-      )
+    format_uuid(hex(16))
   end
 end
 
 # The Guid class is what it says on the packet, but you can assert a :new one.
 class Guid
+  SEQ_FILE_NAME = "/tmp/ActiveFactsRandom"
+  @@sequence = nil
   def initialize(i = :new)
+    @@sequence = ENV['ACTIVEFACTS_RANDOM'] || false if @@sequence == nil
     if i == :new
-      @value = SecureRandom.uuid.freeze
+      case @@sequence
+      when 'fixed'
+	@@counter ||= 0
+	@value = SecureRandom.format_uuid('%032x' % (@@counter += 1))
+      when 'record'
+	@@sequence_file ||= File.open(SEQ_FILE_NAME, 'w')
+	@value = SecureRandom.uuid.freeze
+	@@sequence_file.puts(@value)
+      when 'replay'
+	@@sequence_file ||= File.open(SEQ_FILE_NAME, 'r')
+	@value = @@sequence_file.gets.chomp
+      else
+	@value = SecureRandom.uuid.freeze
+      end
     elsif (v = i.to_s).length == 36 and !(v !~ /[^0-9a-f]/i)
       @value = v.clone.freeze
     else
